655cc7eef855f7223b66f0228960edf7909238de
Harish Gujja <g.harish247@gmail.com>
2017-02-16 01:02:59 -0800

    images to jpg

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,12 @@ module.exports = function(grunt){
                 // use: [mozjpeg()]
             },
             files: {                         // Dictionary of files 
-                'img/2048_opt.png': 'img/2048.png', // 'destination': 'source' 
-                'img/cam_be_like_opt.png': 'img/cam_be_like.jpg',
-                'img/mobilewebdev_opt.png':'img/mobilewebdev.jpg',
-                'img/profilepic_opt.png':'img/profilepic.jpg',
-                'views/images/pizza_opt.png':'views/images/pizza.png',
-                'views/images/pizzeria_opt.png':'views/images/pizzeria.jpg'
+                'img/2048_opt.jpg': 'img/2048.png', // 'destination': 'source' 
+                'img/cam_be_like_opt.jpg': 'img/cam_be_like.jpg',
+                'img/mobilewebdev_opt.jpg':'img/mobilewebdev.jpg',
+                'img/profilepic_opt.jpg':'img/profilepic.jpg',
+                'views/images/pizza_opt.jpg':'views/images/pizza.png',
+                'views/images/pizzeria_opt.jpg':'views/images/pizzeria.jpg'
                 }
             }
         }
